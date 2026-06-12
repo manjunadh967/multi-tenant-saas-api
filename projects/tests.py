3e12d9ad1a963546b17e59bd1c2846106093485a
Manju <manjunadh967@gmail.com>
2026-06-12 13:11:22 +0530

    Update CI workflow with tests

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+from accounts.models import User
+from organizations.models import Organization
+
+
+class ProjectAPITest(APITestCase):
+
+    def setUp(self):
+        self.organization = Organization.objects.create(name='Test Company')
+        self.user = User.objects.create(
+            email='Test@email.com',
+            username='Test',
+            organization=self.organization,
+            role='admin'
+        )
+        self.user.set_password('test@123')
+        self.user.save()
+        
+        from rest_framework_simplejwt.tokens import RefreshToken
+
+        refresh = RefreshToken.for_user(self.user)
+
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f'Bearer {refresh.access_token}'
+        )
+        
+
+    def test_create_project(self):
+        response = self.client.post('/api/projects/',
+                                        {'name': 'Mobile App',
+                                        'description': 'Build company mobile app'
+                                        }, format='json'
+                                    )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,60 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+from organizations.models import Organization
+from accounts.models import User
+from projects.models import Project
+from tasks.models import Task
+
+
+class TaskPermissionTest(APITestCase):
+
+    def setUp(self):
+
+        self.organization = Organization.objects.create(
+            name='Test Company'
+        )
+
+        self.user = User.objects.create_user(
+            email='user@test.com',
+            password='password123',
+            username='Normal User',
+            organization=self.organization,
+            role='member'
+        )
+
+        self.project = Project.objects.create(
+            name='Project',
+            organization=self.organization,
+            created_by=self.user
+        )
+
+        self.task = Task.objects.create(
+            title='Task 1',
+            project=self.project
+        )
+
+        response = self.client.post(
+            '/api/login/',
+            {
+                'email': 'user@test.com',
+                'password': 'password123'
+            }
+        )
+
+        token = response.data['access']
+
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f'Bearer {token}'
+        )
+
+    def test_member_cannot_delete_task(self):
+
+        response = self.client.delete(
+            f'/api/tasks/{self.task.id}/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_403_FORBIDDEN
+        )
